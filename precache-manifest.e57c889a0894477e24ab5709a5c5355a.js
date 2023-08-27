34a2109b64bd94fd4b31dbd0526e75f97afa7749
loki52501 <artcreativity2019@gmail.com>
2023-08-27 23:24:53 +0530

    Updates

---
+++ precache-manifest.e57c889a0894477e24ab5709a5c5355a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c92bf593182f5e0eac4b",
+    "url": "/lks_site/static/css/main.3303d149.chunk.css"
+  },
+  {
+    "revision": "c92bf593182f5e0eac4b",
+    "url": "/lks_site/static/js/main.c92bf593.chunk.js"
+  },
+  {
+    "revision": "619ede3144beb078d66b",
+    "url": "/lks_site/static/css/1.c60957ca.chunk.css"
+  },
+  {
+    "revision": "619ede3144beb078d66b",
+    "url": "/lks_site/static/js/1.619ede31.chunk.js"
+  },
+  {
+    "revision": "6cec07da15cf7e8aabf7",
+    "url": "/lks_site/static/js/runtime~main.6cec07da.js"
+  },
+  {
+    "revision": "da927c69ec71d2c66e46f3c06d307be7",
+    "url": "/lks_site/index.html"
+  }
+];
